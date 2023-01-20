87a8bac7d32310ffd4d18708a3a5f284d5040e6d
aimalot <89665691+aimaIot@users.noreply.github.com>
2023-01-20 22:15:08 +0100

    Update index.php

--- Enkelwebbsida/index.php
+++ Enkelwebbsida/index.php
@@ -18,13 +18,21 @@
     </header>
 
     <nav>
-        
-    </nav>
+    <form method="GET">
+        <input type="hidden" name="site" value="calculator">
+        <button type="submit">Calculator</button>
+    </form>
 
-    <main>
-    <label for="CalculatorBoxOne">Number One</label>
-        <input type="text" name="CalculatorBoxOne" value="">
-    </main>
+    <form method="GET">
+        <input type="hidden" name="site" value="daysLived">
+        <button type="submit">Days Lived</button>
+    </form>
+
+    <form method="GET">
+        <input type="hidden" name="site" value="site2">
+        <button type="submit">Site3</button>
+    </form>
+    </nav>
 
 </body>
 </html>
@@ -33,5 +41,46 @@
 <?php
 
 
+$calculatorHTML = '
+<main>
+<label for="CalculatorBoxOne">Number One</label>
+<input type="text" name="CalculatorBoxOne" value="">
+</main>
+';
 
+$daysLivedHTML = '
+<main class="DaysLivedMain">
+<form method="GET">
+    <label for="birthdate">Birthday (YYYY/MM/DD)</label>
+    <br></br>
+    <input type="text" name="birthdate" placeholder="YYYY/MM/DD">
+    <input type="submit" name="submit_birthdate" value="Submit">
+</form>
+</main>
+';
+
+if (isset($_GET["site"])){
+    if($_GET["site"] == "calculator"){
+        echo $calculatorHTML;
+    }
+    elseif($_GET["site"] == "daysLived"){
+        echo $daysLivedHTML;
+    }
+}
+
+
+// PAGE HANDLERS \\
+
+// CALCULATOR
+
+// DAYS LIVED
+if(isset($_GET["submit_birthdate"])){
+    $birthdate = $_GET["birthdate"];
+
+    $totalDays = date("Y") * 365.24 + date("m") * 30.4 + date("d");
+    $birthdateArray = explode("/", $birthdate);
+    $daysAlive = floor($totalDays - ($birthdateArray[0] * 365.24 + $birthdateArray[1] * 30.4 + $birthdateArray[2]));
+
+    echo "<p class=DaysLivedp>You have been alive for " . $daysAlive . " days!</p>";
+}
 ?>
