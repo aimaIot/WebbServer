0ff4689e76d866530948e013fef8971c3e5f96e0
aimalot <89665691+aimaIot@users.noreply.github.com>
2023-01-20 13:22:29 +0100

    Add files via upload

---
+++ Enkelwebbsida/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tools</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@500&display=swap" rel="stylesheet">
+    
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Tools</h1>
+    </header>
+
+    <nav>
+        
+    </nav>
+
+    <main>
+    <label for="CalculatorBoxOne">Number One</label>
+        <input type="text" name="CalculatorBoxOne" value="">
+    </main>
+
+</body>
+</html>
+
+
+<?php
+
+
+
+?>
