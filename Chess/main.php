8993bff1c5ecab452c115f161e3990bcabfbce88
Noah <33662924+JakeWallbets@users.noreply.github.com>
2023-02-10 13:13:51 +0100

    Add files via upload

---
+++ Chess/main.php
@@ -0,0 +1,104 @@
+<html>
+<link rel="stylesheet" href="style.css">
+
+<head>
+
+</head>
+
+<body>
+    <div class="overlay">
+        <div class="chessboard">
+            <div class="row">
+                <button class="square black" id="a8"></button>
+                <button class="square white" id="b8"></button>
+                <button class="square black" id="c8"></button>
+                <button class="square white" id="d8"></button>
+                <button class="square black" id="e8"></button>
+                <button class="square white" id="f8"></button>
+                <button class="square black" id="g8"></button>
+                <button class="square white" id="h8"></button>
+            </div>
+            <div class="row">
+                <button class="square white" id="a7"></button>
+                <button class="square black" id="b7"></button>
+                <button class="square white" id="c7"></button>
+                <button class="square black" id="d7"></button>
+                <button class="square white" id="e7"></button>
+                <button class="square black" id="f7"></button>
+                <button class="square white" id="g7"></button>
+                <button class="square black" id="h7"></button>
+            </div>
+            <div class="row">
+                <button class="square black" id="a6"></button>
+                <button class="square white" id="b6"></button>
+                <button class="square black" id="c6"></button>
+                <button class="square white" id="d6"></button>
+                <button class="square black" id="e6"></button>
+                <button class="square white" id="f6"></button>
+                <button class="square black" id="g6"></button>
+                <button class="square white" id="h6"></button>
+            </div>
+            <div class="row">
+                <button class="square white" id="a5"></button>
+                <button class="square black" id="b5"></button>
+                <button class="square white" id="c5"></button>
+                <button class="square black" id="d5"></button>
+                <button class="square white" id="e5"></button>
+                <button class="square black" id="f5"></button>
+                <button class="square white" id="g5"></button>
+                <button class="square black" id="h5"></button>
+            </div>
+            <div class="row">
+                <button class="square black" id="a4"></button>
+                <button class="square white" id="b4"></button>
+                <button class="square black" id="c4"></button>
+                <button class="square white" id="d4"></button>
+                <button class="square black" id="e4"></button>
+                <button class="square white" id="f4"></button>
+                <button class="square black" id="g4"></button>
+                <button class="square white" id="h4"></button>
+            </div>
+            <div class="row">
+                <button class="square white" id="a3"></button>
+                <button class="square black" id="b3"></button>
+                <button class="square white" id="c3"></button>
+                <button class="square black" id="d3"></button>
+                <button class="square white" id="e3"></button>
+                <button class="square black" id="f3"></button>
+                <button class="square white" id="g3"></button>
+                <button class="square black" id="h3"></button>
+            </div>
+            <div class="row">
+                <button class="square black" id="a2"></button>
+                <button class="square white" id="b2"></button>
+                <button class="square black" id="c2"></button>
+                <button class="square white" id="d2"></button>
+                <button class="square black" id="e2"></button>
+                <button class="square white" id="f2"></button>
+                <button class="square black" id="g2"></button>
+                <button class="square white" id="h2"></button>
+            </div>
+            <div class="row">
+                <button class="square white" id="a1"></button>
+                <button class="square black" id="b1"></button>
+                <button class="square white" id="c1"></button>
+                <button class="square black" id="d1"></button>
+                <button class="square white" id="e1"></button>
+                <button class="square black" id="f1"></button>
+                <button class="square white" id="g1"></button>
+                <button class="square black" id="h1"></button>
+            </div>
+        </div>
+<?php
+
+$bishop;
+$rook;
+$
+
+
+
+?>
+        
+    </div>
+
+</body>
