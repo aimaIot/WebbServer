2584ba573bcf87ba746cef687aec3ce776beb942
aimalot <89665691+aimaIot@users.noreply.github.com>
2023-03-14 22:08:56 +0100

    Add files via upload

---
+++ Twitter/includes/db-inc.php
@@ -0,0 +1,7 @@
+<?php
+
+$pdo = new PDO('sqlite:data.db');
+
+// $statement = $pdo->query("SELECT * FROM users");
+
+// $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
---
+++ Twitter/index.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+include_once 'header.php';
+
+
+
+
+?>
